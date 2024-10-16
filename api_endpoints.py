bc4c21c2b11e143137784b8dd529640cd6db5cd1
Inuwa Mobarak Abraham <inuwamobarak@gmail.com>
2024-10-16 13:47:09 -0700

    Update api_endpoints.py

--- api_endpoints.py
+++ api_endpoints.py
@@ -26,7 +26,7 @@ class ImageRequest(BaseModel):
 @app.post("/generate_image")
 async def generate_image(image_request: ImageRequest):
     try:
-        api_key = "SG_8273f7aa361a8a79"
+        api_key = "your_key_from_segmind"
         api = SegMindAPI(api_key)
         summarizer = TextSummarizer(image_request.url, image_request.num_sentences)
         summary = summarizer.summarize()
@@ -42,7 +42,7 @@ async def generate_image(image_request: ImageRequest):
 @app.post("/generate_image_caption")
 async def generate_image(image_request: ImageRequest):
     try:
-        api_key = "SG_8273f7aa361a8a79"
+        api_key = "your_key_from_segmind"
         api = SegMindAPI(api_key)
         summarizer = TextSummarizer(image_request.url, image_request.num_sentences)
         summary = summarizer.summarize()
